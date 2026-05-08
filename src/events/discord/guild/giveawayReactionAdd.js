7a70ba2518e48649755cce1e7a9ce539b2e00d0e
AeroX <aeroxdevs@gmail.com>
2026-05-08 13:48:57 +0530

    fix: use GWAY_EMOJI_RAW_ID for reaction add comparison

--- src/events/discord/guild/giveawayReactionAdd.js
+++ src/events/discord/guild/giveawayReactionAdd.js
@@ -1,13 +1,13 @@
 // Falcron | AeroX Development
 // Author: itsfizys
-import { giveawayStore, GWAY_EMOJI_ID } from '#giveawayUtils';
+import { giveawayStore, GWAY_EMOJI_RAW_ID } from '#giveawayUtils';
 
 export default {
         name: 'messageReactionAdd',
         async execute({ eventArgs }) {
                 const [reaction, user] = eventArgs;
 
-                if (reaction.emoji.id !== GWAY_EMOJI_ID) return;
+                if (reaction.emoji.id !== GWAY_EMOJI_RAW_ID) return;
 
                 if (reaction.partial) {
                         await reaction.fetch().catch(() => null);

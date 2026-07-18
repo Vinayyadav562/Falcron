c0e808167de0dc734cd2050ba13c4ef3c95a62db
Vinayyadav562 <vedv34867@gmail.com>
2026-07-18 17:46:30 +0530

    Update Discord token, clientId, and prefix
    Updated Discord configuration values with hardcoded tokens and IDs.

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,15 @@
 // Falcron | AeroX Development
 // Author: itsfizys
 export const config = {
-        token: process.env.DISCORD_TOKEN || '',
-        clientId: process.env.DISCORD_CLIENT_ID || '',
-        prefix: '-',
+        token: process.env.DISCORD_TOKEN || 'MTUyODAwOTgxNjgxODA1NzI5Ng.G2Dady.SjbdZ9XfeVxxdJZU2Gjti_ju-V3l4mInX5VyQY',
+        clientId: process.env.DISCORD_CLIENT_ID || '1528009816818057296',
+        prefix: '$',
         ownerIds: process.env.OWNER_IDS ? process.env.OWNER_IDS.split(',') : [],
         ownerOnly: false,
 
         links: {
                 supportServer: 'https://discord.gg/Ez4gCJQDxB',
-                invite: '',
+                invite: 'https://discord.com/oauth2/authorize?client_id=1528009816818057296',
         },
 
         cache: {
